aaf7b26dbe6b2d41916442e8ee5d7d9781445d68
Canyon <canyonlowes@gmail.com>
2024-10-26 21:58:03 -0400

    Update window-poker-1.py
    Put the test_bet function into the blind function
    added flop
    made blind show 3 cards
    had mad sex with mj

--- project1/window-poker-1.py
+++ project1/window-poker-1.py
@@ -27,43 +27,57 @@ def clear_page(): #clears page when called. (Time saver)
         widget.pack_forget()
         
 
-def test_bet(bet): #checks if your bet is allowed (<= wallet and int)
-    global wallet
-    try:
-        bet_amount = int(bet) #checks if bet can be made an int
-        if bet_amount > wallet: #bet more than you have
-            not_enough_label = tk.Label(window, text = f'Not enough money. Max bet ${wallet}', font=('georgia', 10))
-            not_enough_label.pack(pady=20)
-            window.after(2000, not_enough_label.destroy)
-        else: #bet less than or equal to what you have
-            wallet = wallet - bet_amount
-            total_betting_label = tk.Label(window, text = f'Betting ${bet_amount}. ${wallet} remaining', font = ('georgia', 10))
-            total_betting_label.pack (pady = 20)
-            window.after(2000, total_betting_label.destroy)
-            window.after(200)
-    except: #bet was either a decimal or text
-        error_label = tk.Label(window, text = 'Invalid number', font = ('georgia', 20)) #bet cannot be made an int
-        error_label.pack(pady = 20)
-        window.after(2000, error_label.destroy)
 
+def blind(): #first deal
+    def test_bet(bet,next): #checks if your bet is allowed (<= wallet and int)
+        global wallet
+        try:
+            bet_amount = int(bet) #checks if bet can be made an int
+            if bet_amount > wallet: #bet more than you have
+                not_enough_label = tk.Label(window, text = f'Not enough money. Max bet ${wallet}', font=('georgia', 10))
+                not_enough_label.pack(pady=20)
+                window.after(2000, not_enough_label.destroy)
+            else: #bet less than or equal to what you have
+                wallet = wallet - bet_amount
+                total_betting_label = tk.Label(window, text = f'Betting ${bet_amount}. ${wallet} remaining', font = ('georgia', 10))
+                total_betting_label.pack (pady = 20)
+                window.after(1000, total_betting_label.destroy)
+                window.after(1000, lambda:next())
+        except: #bet was either a decimal or text
+            error_label = tk.Label(window, text = 'Invalid number', font = ('georgia', 20)) #bet cannot be made an int
+            error_label.pack(pady = 20)
+            window.after(2000, error_label.destroy)
 
-
-def deal1(): #first deal
+    def flop():
+        bet_enter_label.destroy()
+        bet_amount_entry.destroy()
+        bet_button.destroy()
+        fold_button.destroy()
+        drawn_cards_2 = random.sample(deck, 1)
+        for card in drawn_cards_2:
+            deck.remove(card)
+        cards_label_2 = tk.Label(window, text=", ".join(drawn_cards_2), font=('Georgia', 20))
+        cards_label_2.pack(pady=0)
+    
     for widget in window.winfo_children():
         widget.pack_forget()
-    drawn_cards = random.sample(deck, 2)
+    drawn_cards = random.sample(deck, 3)
     for card in drawn_cards:
         deck.remove(card) 
-    cards_label = tk.Label(window, text=", ".join(drawn_cards), font=('Georgia', 20))
-    cards_label.pack(pady=20)
+    cards_label = tk.Label(window, text="\n".join(drawn_cards), font=('Georgia', 20))
+    cards_label.pack(pady=0)
     bet_enter_label = tk.Label(window, text = 'enter bet', font = ('georgia', 20))
     bet_enter_label.pack(pady = 20)
     bet_amount_entry = tk.Entry(window, font = ('georgia', 20))
     bet_amount_entry.pack(pady=20)
-    bet_button = tk.Button(window, text = 'BET!', font = ('georgia', 20), command=lambda:test_bet(bet_amount_entry.get()))
+    bet_button = tk.Button(window, text = 'BET!', font = ('georgia', 20), command=lambda:test_bet(bet_amount_entry.get(),next = flop))
     bet_button.pack(pady=0)
     fold_button = tk.Button(window, text = 'Fold', font = ('georgia', 15), command=lambda:fold())
     fold_button.pack(pady=0)
+
+
+
+
 
 def fold (): #if player folds
     wallet_fold_label = tk.Label(window, text = f'${wallet} remaining', font = ('georgia', 10))
@@ -81,14 +95,13 @@ def round_pre_bet(): #Before you bet after each round after first round
     deal_button.pack(pady = 20)
 
 
-
 def pre_bet (name): #before you bet after welcome function. 
     username = str(name)
     for widget in window.winfo_children():
         widget.pack_forget()
     name_label = tk.Label(window, text = f'welcome {name}', font = ('georgia', 30))
     name_label.pack(pady=20)
-    deal_button = tk.Button(window, text = 'DEAL!', font = ('georgia', 20), command=lambda:deal1())
+    deal_button = tk.Button(window, text = 'DEAL!', font = ('georgia', 20), command=lambda:blind())
     deal_button.pack(pady=20)
 
     
