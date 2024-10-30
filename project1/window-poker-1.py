bddb5a8eb6bc87d664af58b6ffbf2e31e86c8b64
Canyon <canyonlowes@gmail.com>
2024-10-29 21:37:36 -0400

    Update window-poker-1.py

--- project1/window-poker-1.py
+++ project1/window-poker-1.py
@@ -7,24 +7,28 @@ deck = [f'{rank}of {suit}' for rank in card_types for suit in suites]
 
 import tkinter as tk
 window = tk.Tk()
-window.configure(bg='green4')
 window.geometry("500x500")
 window.title('Poker')
 
+selected_color = ('green')
 
 wallet = int(100)
 
-def welcome (color): #welcome screen. (Add settings button and more stuff here)
+
+def welcome (): #welcome screen. (Add settings button and more stuff here)
+    window.configure(bg=selected_color)
     clear_page()
-    window.configure(bg=color)
     welcome_label = tk.Label(window, text = "Enter name below", font = ("georgia", 20))
     welcome_label.pack(pady=20)
     name_entry = tk.Entry(window, bg='brown4', fg='black', font = ("georgia", 20))
     name_entry.pack(pady=30)
+    username = (name_entry.get())
     play_button = tk.Button(window, bg="red4", fg='black', text = "Play!", font = ("georgia", 20),command=lambda: pre_bet(name=name_entry.get()))
     play_button.pack(pady=40)
     settings_button = tk.Button(window, text = 'Settings', font = ('georgia', 20), command=lambda:settings_page())
     settings_button.pack(pady=20)
+    wallet_label = tk.Label(window, text = f'Wallet: ${wallet}')
+    wallet_label.pack(pady=20)
 
 def clear_page(): #clears page when called. (Time saver)
     for widget in window.winfo_children():
@@ -33,6 +37,7 @@ def clear_page(): #clears page when called. (Time saver)
 
 
 def blind(): #first deal
+    global current_color
     def test_bet(bet,next): #checks if your bet is allowed (<= wallet and int)
         global wallet
         try:
@@ -62,9 +67,21 @@ def blind(): #first deal
             deck.remove(card)
         cards_label_2 = tk.Label(window, text=", ".join(drawn_cards_2), font=('Georgia', 20))
         cards_label_2.pack(pady=0)
+        bet_entry_flop = tk.Entry(window, font = ('georgia', 20))
+        bet_entry_flop.pack(pady=20)
+        bet_button_flop = tk.Button(window, text = 'BET!', font = ('georgia', 20), command=lambda:test_bet(bet_entry_flop.get(),next = turn))
+        bet_button_flop.pack(pady=0)
+        fold_button_flop = tk.Button(window, text = 'Fold', font = ('georgia', 15), command=lambda:fold())
+        fold_button_flop.pack(pady=0)
     
-    for widget in window.winfo_children():
-        widget.pack_forget()
+    def turn():
+        clear_page()
+        print ('placeholder')
+        print (wallet)
+
+
+    
+    clear_page()
     drawn_cards = random.sample(deck, 3)
     for card in drawn_cards:
         deck.remove(card) 
@@ -84,12 +101,13 @@ def blind(): #first deal
 
 
 def fold (): #if player folds
+    global current_color
     wallet_fold_label = tk.Label(window, text = f'${wallet} remaining', font = ('georgia', 10))
     wallet_fold_label.pack(pady=20)
     folding_lable=tk.Label(window, text = 'Folding', font = ('georgia', 10))
     folding_lable.pack(pady=20)
     window.after(2,lambda:clear_page())
-    window.after(1000, lambda:round_pre_bet())
+    window.after(1000, lambda:welcome())
 
 '''was having trouble getting the pre_bet function to work. Could'nt get it to carry the username variable. Might try to fix, but
 this should work for now'''
@@ -101,9 +119,9 @@ def round_pre_bet(): #Before you bet after each round after first round
 
 def pre_bet (name): #before you bet after welcome function. 
     username = str(name)
-    for widget in window.winfo_children():
-        widget.pack_forget()
-    name_label = tk.Label(window, text = f'welcome {name}', font = ('georgia', 30))
+    print (username) #testing
+    clear_page()
+    name_label = tk.Label(window, text = f'welcome {username}', font = ('georgia', 30))
     name_label.pack(pady=20)
     deal_button = tk.Button(window, text = 'DEAL!', font = ('georgia', 20), command=lambda:blind())
     deal_button.pack(pady=20)
@@ -116,13 +134,16 @@ def settings_page(): #settings page
     back_from_settings_button.pack(pady=20)
     color_pick_test = tk.Entry(window, font = ('georgia', 20))
     color_pick_test.pack(pady=20)
-    commit_button = tk.Button (window, text = 'commit', font = ('georgia', 20), command=lambda:welcome(color_pick_test.get()))
+    commit_button = tk.Button (window, text = 'commit', font = ('georgia', 20), command=lambda:commit_color(color_pick_test.get()))
     commit_button.pack(pady=20)
 
-    
+def commit_color(color):
+    global selected_color
+    selected_color = color
+    print (f'running, {selected_color}') #testing purposes
+    welcome()
 
 
-
-welcome('green')
+welcome()
 
 window.mainloop()
