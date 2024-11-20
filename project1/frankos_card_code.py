6e3ed0c27e51a386f87fd8dac162514e2cef734b
Canyon Lowes <canyonlowes@gmail.com>
2024-11-19 21:18:47 -0500

    i did stuff-things mayh or may not work. :)

---
+++ project1/frankos_card_code.py
@@ -0,0 +1,107 @@
+if card_types == 'Ace' and suits == 'hearts':
+    image = Image.open('ace_of_hearts.png')
+if card_types == 'Ace' and suits == 'spades':
+    image = Image.open('ace_of_spades2.png')
+if card_types == 'Ace' and suits == 'diamonds':
+    image = Image.open('ace_of_diamonds.png')
+if card_types == 'Ace' and suits == 'clubs':
+    image = Image.open('ace_of_clubs.png')
+
+if card_types == 'Jack' and suits == 'hearts':
+    image = Image.open('jack_of_hearts2.png')
+if card_types == 'Jack' and suits == 'spades':
+    image = Image.open('jack_of_spades2.png')
+if card_types == 'Jack' and suits == 'diamonds':
+    image = Image.open('jack_of_diamonds2.png')
+if card_types == 'Jack' and suits == 'clubs':
+    image = Image.open('jack_of_clubs2.png')
+        
+if card_types == 'King' and suits == 'hearts':
+    image = Image.open('king_of_hearts2.png')
+if card_types == 'King' and suits == 'spades':
+    image = Image.open('king_of_spades2.png')
+if card_types == 'King' and suits == 'diamonds':
+    image = Image.open('king_of_diamonds2.png')
+if card_types == 'King' and suits == 'clubs':
+    image = Image.open('king_of_clubs2.png')
+
+if card_types == '2' and suits == 'hearts':
+    image = Image.open('2_of_hearts.png')
+if card_types == '2' and suits == 'spades':
+    image = Image.open('2_of_spades.png')
+if card_types == '2' and suits == 'diamonds':
+    image = Image.open('2_of_diamonds.png')
+if card_types == '2' and suits == 'clubs':
+    image = Image.open('2_of_clubs.png')
+        
+if card_types == '3' and suits == 'hearts':
+    image = Image.open('3_of_hearts.png')
+if card_types == '3' and suits == 'spades':
+    image = Image.open('3_of_spades.png')
+if card_types == '3' and suits == 'diamonds':
+    image = Image.open('3_of_diamonds.png')
+if card_types == '3' and suits == 'clubs':
+    image = Image.open('3_of_clubs.png')
+
+if card_types == '4' and suits == 'hearts':
+    image = Image.open('4_of_hearts.png')
+if card_types == '4' and suits == 'spades':
+    image = Image.open('4_of_spades.png')
+if card_types == '4' and suits == 'diamonds':
+    image = Image.open('4_of_diamonds.png')
+if card_types == '4' and suits == 'clubs':
+    image = Image.open('4_of_clubs.png')
+
+if card_types == '5' and suits == 'hearts':
+    image = Image.open('5_of_hearts.png')
+if card_types == '5' and suits == 'spades':
+    image = Image.open('5_of_spades.png')
+if card_types == '5' and suits == 'diamonds':
+    image = Image.open('5_of_diamonds.png')
+if card_types == '5' and suits == 'clubs':
+    image = Image.open('5_of_clubs.png')
+        
+if card_types == '6' and suits == 'hearts':
+    image = Image.open('6_of_hearts.png')
+if card_types == '6' and suits == 'spades':
+    image = Image.open('6_of_spades.png')
+if card_types == '6' and suits == 'diamonds':
+    image = Image.open('6_of_diamonds.png')
+if card_types == '6' and suits == 'clubs':
+    image = Image.open('6_of_clubs.png')
+
+if card_types == '7' and suits == 'hearts':
+    image = Image.open('7_of_hearts.png')
+if card_types == '7' and suits == 'spades':
+    image = Image.open('7_of_spades.png')
+if card_types == '7' and suits == 'diamonds':
+    image = Image.open('7_of_diamonds.png')
+if card_types == '7' and suits == 'clubs':
+    image = Image.open('7_of_clubs.png')
+
+if card_types == '8' and suits == 'hearts':
+    image = Image.open('8_of_hearts.png')
+if card_types == '8' and suits == 'spades':
+    image = Image.open('8_of_spades.png')
+if card_types == '8' and suits == 'diamonds':
+    image = Image.open('8_of_diamonds.png')
+if card_types == '8' and suits == 'clubs':
+    image = Image.open('8_of_clubs.png')
+
+if card_types == '9' and suits == 'hearts':
+    image = Image.open('9_of_hearts.png')
+if card_types == '9' and suits == 'spades':
+    image = Image.open('9_of_spades.png')
+if card_types == '9' and suits == 'diamonds':
+    image = Image.open('9_of_diamonds.png')
+if card_types == '9' and suits == 'clubs':
+    image = Image.open('9_of_clubs.png')
+
+if card_types == '10' and suits == 'hearts':
+    image = Image.open('10_of_hearts.png')
+if card_types == '10' and suits == 'spades':
+    image = Image.open('10_of_spades.png')
+if card_types == '10' and suits == 'diamonds':
+    image = Image.open('10_of_diamonds.png')
+if card_types == '10' and suits == 'clubs':
+    image = Image.open('10_of_clubs.png')
--- project1/window-poker-1.py
+++ project1/window-poker-1.py
@@ -1,17 +1,20 @@
 import random
 
-card_types = (['Ace ','1 ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','10 ','Jack ','Queen ','King '])
+card_types = (['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'])
 suits = (['hearts','diamonds','spades','clubs'])
-card = random.choice (card_types) + random.choice (suits)
 deck = [f'{rank}of {suit}' for rank in card_types for suit in suits]
+#dictionary to map card names to image files
+card_images = {}
+
+# Populates the dictionary with the card images
+for rank in card_types:
+    for suit in suits:
+        card_images[f'{rank} of {suit}'] = f'{rank.lower()}_of_{suit}.png'
 
 import tkinter as tk
 window = tk.Tk()
 window.geometry("500x500")#change to make full screen before relese (or do something with settings)
 window.title('Poker')
-gold_icon = 'gold_icon1.ico'
-window.iconbitmap(gold_icon)
-
 
 wallet = int(100)
 
@@ -33,113 +36,7 @@ selected_theme = gold
 home_background = gold
 
 
-if card_types == 'Ace' and suits == 'hearts':
-    image = Image.open('ace_of_hearts.png')
-if card_types == 'Ace' and suits == 'spades':
-    image = Image.open('ace_of_spades2.png')
-if card_types == 'Ace' and suits == 'diamonds':
-    image = Image.open('ace_of_diamonds.png')
-if card_types == 'Ace' and suits == 'clubs':
-    image = Image.open('ace_of_clubs.png')
-
-if card_types == 'Jack' and suits == 'hearts':
-    image = Image.open('jack_of_hearts2.png')
-if card_types == 'Jack' and suits == 'spades':
-    image = Image.open('jack_of_spades2.png')
-if card_types == 'Jack' and suits == 'diamonds':
-    image = Image.open('jack_of_diamonds2.png')
-if card_types == 'Jack' and suits == 'clubs':
-    image = Image.open('jack_of_clubs2.png')
-        
-if card_types == 'King' and suits == 'hearts':
-    image = Image.open('king_of_hearts2.png')
-if card_types == 'King' and suits == 'spades':
-    image = Image.open('king_of_spades2.png')
-if card_types == 'King' and suits == 'diamonds':
-    image = Image.open('king_of_diamonds2.png')
-if card_types == 'King' and suits == 'clubs':
-    image = Image.open('king_of_clubs2.png')
-
-if card_types == '2' and suits == 'hearts':
-    image = Image.open('2_of_hearts.png')
-if card_types == '2' and suits == 'spades':
-    image = Image.open('2_of_spades.png')
-if card_types == '2' and suits == 'diamonds':
-    image = Image.open('2_of_diamonds.png')
-if card_types == '2' and suits == 'clubs':
-    image = Image.open('2_of_clubs.png')
-        
-if card_types == '3' and suits == 'hearts':
-    image = Image.open('3_of_hearts.png')
-if card_types == '3' and suits == 'spades':
-    image = Image.open('3_of_spades.png')
-if card_types == '3' and suits == 'diamonds':
-    image = Image.open('3_of_diamonds.png')
-if card_types == '3' and suits == 'clubs':
-    image = Image.open('3_of_clubs.png')
-
-if card_types == '4' and suits == 'hearts':
-    image = Image.open('4_of_hearts.png')
-if card_types == '4' and suits == 'spades':
-    image = Image.open('4_of_spades.png')
-if card_types == '4' and suits == 'diamonds':
-    image = Image.open('4_of_diamonds.png')
-if card_types == '4' and suits == 'clubs':
-    image = Image.open('4_of_clubs.png')
-
-if card_types == '5' and suits == 'hearts':
-    image = Image.open('5_of_hearts.png')
-if card_types == '5' and suits == 'spades':
-    image = Image.open('5_of_spades.png')
-if card_types == '5' and suits == 'diamonds':
-    image = Image.open('5_of_diamonds.png')
-if card_types == '5' and suits == 'clubs':
-    image = Image.open('5_of_clubs.png')
-        
-if card_types == '6' and suits == 'hearts':
-    image = Image.open('6_of_hearts.png')
-if card_types == '6' and suits == 'spades':
-    image = Image.open('6_of_spades.png')
-if card_types == '6' and suits == 'diamonds':
-    image = Image.open('6_of_diamonds.png')
-if card_types == '6' and suits == 'clubs':
-    image = Image.open('6_of_clubs.png')
-
-if card_types == '7' and suits == 'hearts':
-    image = Image.open('7_of_hearts.png')
-if card_types == '7' and suits == 'spades':
-    image = Image.open('7_of_spades.png')
-if card_types == '7' and suits == 'diamonds':
-    image = Image.open('7_of_diamonds.png')
-if card_types == '7' and suits == 'clubs':
-    image = Image.open('7_of_clubs.png')
-
-if card_types == '8' and suits == 'hearts':
-    image = Image.open('8_of_hearts.png')
-if card_types == '8' and suits == 'spades':
-    image = Image.open('8_of_spades.png')
-if card_types == '8' and suits == 'diamonds':
-    image = Image.open('8_of_diamonds.png')
-if card_types == '8' and suits == 'clubs':
-    image = Image.open('8_of_clubs.png')
-
-if card_types == '9' and suits == 'hearts':
-    image = Image.open('9_of_hearts.png')
-if card_types == '9' and suits == 'spades':
-    image = Image.open('9_of_spades.png')
-if card_types == '9' and suits == 'diamonds':
-    image = Image.open('9_of_diamonds.png')
-if card_types == '9' and suits == 'clubs':
-    image = Image.open('9_of_clubs.png')
-
-if card_types == '10' and suits == 'hearts':
-    image = Image.open('10_of_hearts.png')
-if card_types == '10' and suits == 'spades':
-    image = Image.open('10_of_spades.png')
-if card_types == '10' and suits == 'diamonds':
-    image = Image.open('10_of_diamonds.png')
-if card_types == '10' and suits == 'clubs':
-    image = Image.open('10_of_clubs.png')
+#this is where frankos card code was
 
 
 
@@ -254,18 +151,31 @@ def blind(): #first deal
             bet_entry_turn = tk.Entry(window, font = ('georgia', 20))
             bet_entry_turn.pack(pady=20)
             bet_button_turn = tk.Button(window, text = 'BET!', font = ('georgia', 20), command= lambda:test_bet(bet_entry_turn.get()))
+            bet_button_turn.pack(pady=20)
 
             def final():
                 print('final placeholder :)')
 
 
 
-    clear_page()
+    clear_page() 
+    
     drawn_cards = random.sample(deck, 3)
+
     for card in drawn_cards:
-        deck.remove(card) 
-    cards_label = tk.Label(window, text="\n".join(drawn_cards), font=('Georgia', 20))
-    cards_label.pack(pady=0)
+        deck.remove(card)
+        image_file = card_images.get(card, None)
+        
+        if image_file:
+            image = Image.open(image_file)
+            image = image.resize((60, 85))
+            photo = ImageTk.PhotoImage(image)
+            
+            # Create a label to display the image
+            image_label = tk.Label(window, image=photo)
+            image_label.image = photo  # Keep a reference to the image
+            image_label.pack()
+
     bet_enter_label = tk.Label(window, text = 'enter bet', font = ('georgia', 20))
     bet_enter_label.pack(pady = 20)
     bet_amount_entry = tk.Entry(window, font = ('georgia', 20))
